6b1779bc7e4fd983dd7c7401911c0dfda7e19bb0
graecook575@outlook.com <graecook575@outlook.com>
2021-02-09 09:57:52 -0500

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿
+namespace PlayGame
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.playButton = new System.Windows.Forms.Button();
+            this.countdownText = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.White;
+            this.playButton.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(161, 122);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(190, 111);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play Game";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // countdownText
+            // 
+            this.countdownText.BackColor = System.Drawing.Color.Transparent;
+            this.countdownText.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownText.Location = new System.Drawing.Point(197, 161);
+            this.countdownText.Name = "countdownText";
+            this.countdownText.Size = new System.Drawing.Size(118, 37);
+            this.countdownText.TabIndex = 1;
+            this.countdownText.Text = "button1";
+            this.countdownText.UseVisualStyleBackColor = false;
+            this.countdownText.Visible = false;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(504, 370);
+            this.Controls.Add(this.countdownText);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "mainForm";
+            this.Text = "Play Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button countdownText;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,51 @@
+﻿/*Graeme Cook
+ * February 8th, 2021
+ * A program that, when initiated, counts down from three and displays a message, as would be done when starting a game.*/
+
+//Setup
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGame
+{
+    public partial class mainForm : Form
+   {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //On button press, make the button dissapear and another, white, one popup to display the countdown, also setup soundplayer for audio playback
+            playButton.Visible = false;
+            countdownText.Visible = true;
+            countdownText.BackColor = Color.White;
+            SoundPlayer countPlayer = new SoundPlayer(Properties.Resources.Count);
+            SoundPlayer goPlayer = new SoundPlayer(Properties.Resources.Go);
+
+            //Countdown & accompanying sounds as a loop
+            for (int t = 3; t > 0; t--)
+            {
+                countdownText.Text = $"{t}...";
+                countPlayer.Play();
+                Refresh();
+                Thread.Sleep(2000);
+            }
+            
+            //Display "GO" message, change BG colour, and play sound
+            countdownText.Text = "Go!!!";
+            BackColor = Color.SlateBlue;
+            goPlayer.Play();
+        }
+    }
+}
